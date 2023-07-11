6e7428fe3659c41101d0984d217369072ca1b7a3
Teddy K <teddykoerniadi@gmail.com>
2023-07-11 14:33:07 +0700

    create function get meetings

--- src/zoomHelper.js
+++ src/zoomHelper.js
@@ -68,4 +68,32 @@ export default class ZoomHelper {
       throw err;
     }
   }
+
+  async getMeetings(userId, params) {
+    try {
+      const token = await this.generateToken();
+
+      const options = { 
+        page_size: params?.page_size || 50, 
+        page_number: params?.page_number || 1,
+        next_page_token: params?.next_page_token ||  "",
+        from: params?.from ||  "",
+        to: params?.to ||  "" 
+      }
+
+      const { data } = await axios({
+        method: "GET",
+        url: `${this.zoomBaseUrl}/users/${userId}/meetings`,
+        headers: {
+          Authorization: `Bearer ${token.access_token}`,
+        },
+        params: options,
+      });
+
+      return data;
+    } catch (err) {
+      throw err;
+    }
+  }
+
 }
